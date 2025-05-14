fb303c548c7a856b79ec3cc133a254fa6834bdeb
Jonathan <jonathandrumbass@gmail.com>
2025-05-14 19:01:39 -0300

    first commit

---
+++ src/test/java/br/com/dev/cadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.cadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
